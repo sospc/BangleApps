bba48133e714cb4adc401106b4dd0393612a3e61
sospc <sospc1@gmail.com>
2020-04-26 20:45:03 +0100

    Update widget.js

--- apps/widhrm/widget.js
+++ apps/widhrm/widget.js
@@ -8,7 +8,7 @@
   	g.reset();
     g.setFont("6x8", 2);
     g.setFontAlign(0, 0);
-    g.clearRect(this.x,this.y+20,this.x+width,this.y+30); // erase background
+    g.clearRect(this.x,this.y+200,this.x+width,this.y+300); // erase background
     var bpm = currentBPM, isCurrent = true;
     if (bpm===undefined) {
       bpm = lastBPM;
