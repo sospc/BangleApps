a92800deadf2419ea1bba88075f1fbf3c5d54c88
sospc <sospc1@gmail.com>
2020-04-29 02:21:46 +0100

    Update widget.js

--- apps/widbt/widget.js
+++ apps/widbt/widget.js
@@ -4,10 +4,10 @@ var img_bt = E.toArrayBuffer(atob("CxQBBgDgFgJgR4jZMawfAcA4D4NYybEYIwTAsBwDAA=="
 function draw() {
   g.reset();
   if (NRF.getSecurityStatus().connected) {
-    E.showMessage("","ONLINE"); 
+    //E.showMessage("","ONLINE"); 
     g.setColor(0,0.5,1); }
  else {
-    E.showMessage("Equipamento \n Sem cobertura...\n","OFFLINE");
+    //E.showMessage("Equipamento \n Sem cobertura...\n","OFFLINE");
     g.setColor(0.3,0.3,0.3);
     g.drawImage(img_bt,10+this.x,2+this.y); }
 }
